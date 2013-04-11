a0860cf9e01f3b1ed576782e320cc1b06cd09588
Florent Galland <florent.galland@gmail.com>
2013-04-11 17:37:37 +0200

    Add header and version bump

--- glarkconnector.py
+++ glarkconnector.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python
+# Copyright 2013 Florent Galland & Luc Verdier
+#
+# This file is part of glarkconnector.
+#
+# glarkconnector is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# at your option) any later version.
+#
+# glarkconnector is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with glarkconnector.  If not, see <http://www.gnu.org/licenses/>.
 """Connector for the glark.io editor. """
 
-__version__ = "0.1"
+__version__ = "0.2"
 
 import BaseHTTPServer
 import base64
