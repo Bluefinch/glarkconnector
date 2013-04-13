270137a8b142ce09bf6cef65d2f59389446dbdc5
Florent Galland <florent.galland@gmail.com>
2013-04-14 00:27:11 +0200

    Try to fix travis

--- tests/tests.py
+++ tests/tests.py
@@ -84,15 +84,16 @@ class GlarkConnectorTest(unittest.TestCase):
         self.assertIsSuccessfulJsend(res.json())
 
         jsend = res.json()
-        jsend_ref = {"status": "success", "data": [{"path": ".glarkconnector.conf", "type": "file", "name": ".glarkconnector.conf"}, {"path": "subdirectory", "type": "dir", "name": "subdirectory"}, {"path": "subdirectory with spaces", "type": "dir", "name": "subdirectory with spaces"}, {"path": "file1", "type": "file", "name": "file1"}, {"path": "file2", "type": "file", "name": "file2"}]}
+        jsend_ref = {"status": "success", "data": [{"path": "subdirectory with spaces", "type": "dir", "name": "subdirectory with spaces"}, {"path": "file1", "type": "file", "name": "file1"}, {"path": ".glarkconnector.conf", "type": "file", "name": ".glarkconnector.conf"}, {"path": "subdirectory", "type": "dir", "name": "subdirectory"}, {"path": "file2", "type": "file", "name": "file2"}]}
 
         print 'jsend:'
         print jsend
-        print json.dumps(jsend)
+        print json.dumps(jsend, sort_keys=True)
         print 'jsend_ref:'
         print jsend_ref
-        print json.dumps(jsend_ref)
+        print json.dumps(jsend_ref, sort_keys=True)
         print ''
+        self.assertTrue(json.dumps(jsend, sort_keys=True) == json.dumps(jsend_ref, sort_keys=True))
         self.assertTrue(jsend == jsend_ref)
 
     def test_list_dir(self):
@@ -105,12 +106,7 @@ class GlarkConnectorTest(unittest.TestCase):
 
         jsend = res.json()
         jsend_ref = {"status": "success", "data": [{"path": "subdirectory/file1", "type": "file", "name": "file1"}, {"path": "subdirectory/file2", "type": "file", "name": "file2"}]}
-        print 'jsend:'
-        print json.dumps(jsend)
-        print 'jsend_ref:'
-        print json.dumps(jsend_ref)
-        print ''
-        self.assertTrue(json.dumps(jsend) == json.dumps(jsend_ref))
+        self.assertTrue(json.dumps(jsend, sort_keys=True) == json.dumps(jsend_ref, sort_keys=True))
 
     def test_get_file(self):
         res = requests.get(CONNECTOR_URL + '/connector/files/file1', auth=Auth('lucho', 'verYseCure'))
